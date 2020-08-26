b557b7ad1cb3445ea25642a96be72e5405a142ea
renjiyun <wozaijixu@msn.cn>
2020-08-26 13:09:14 +0800

    最基本的aframe组件放置

--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,8 +1,37 @@
 <template>
+  <a-scene>
+    <a-box
+      position="0 2 -5"
+      rotation="0 45 45"
+      scale="1 1 1"
+      animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 2000; loop: true"
+      scale-on-mouseenter
+    ></a-box>
+
+    <a-camera position="0 7 5">
+      <a-cursor color="#FF0000"></a-cursor>
+    </a-camera>
+  </a-scene>
 </template>
 
 <script>
 export default {
+  beforeCreate() {
+    AFRAME.registerComponent("scale-on-mouseenter", {
+      schema: {
+        to: { default: "2.5 2.5 2.5", type: "vec3" }
+      },
+
+      init: function() {
+        var data = this.data;
+        var el = this.el;
+        this.el.addEventListener("click", function() {
+          console.log("=====");
+          el.object3D.scale.copy(data.to);
+        });
+      }
+    });
+  },
   methods: {}
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import App from "./App";
 import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import aframe from "aframe";
 
 Vue.config.productionTip = false;
-
+Vue.use(aframe);
 Vue.use(ElementUI);
 
 new Vue({
